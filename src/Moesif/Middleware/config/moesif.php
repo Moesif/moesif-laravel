8963ac61bbc0d73930660fc48ff9c0c47f296512
xinghengwang <xing@moesif.com>
2020-10-30 12:05:02 -0700

    restored example config/moesif.php file.

---
+++ src/Moesif/Middleware/config/moesif.php
@@ -0,0 +1,12 @@
+<?php
+
+/*
+This config file is used during boot. merely serve as an example/template that
+you can put into your config folder of your laravel project.
+*/
+
+return [
+    'applicationId' => 'your application id',
+    'logBody' => true,
+    'debug' => false
+];
